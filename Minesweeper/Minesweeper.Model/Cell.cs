c2006d54e15315ddd9d644d4c7e20ea8cd4ad52a
igorrozani <igorrozani@gmail.com>
2016-02-25 22:29:10 -0300

    Creating solution, class cell adn tests

---
+++ Minesweeper/Minesweeper.Model/Cell.cs
@@ -0,0 +1,30 @@
+﻿using Minesweeper.Library.Exceptions;
+using System;
+
+namespace Minesweeper.Model
+{
+    public class Cell
+    {
+        public Cell(bool hasBomb)
+        {
+            HasBomb = hasBomb;
+            Status = StatusEnum.Untouched;
+        }
+
+        public bool HasBomb { get; private set; }
+        public StatusEnum Status { get; private set; }
+        public int QuantityBombsNear { get; private set; }
+
+        public void Flagged()
+        {
+            Status = StatusEnum.Flagged;
+        }
+
+        public void Check()
+        {
+            if (HasBomb)
+                throw new GameOverException();
+            Status = StatusEnum.Revealed;
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper.Test/Model/CellTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Model;
+using Minesweeper.Library.Exceptions;
+
+namespace Minesweeper.Test
+{
+    [TestClass]
+    public class CellTest
+    {
+        [TestMethod]
+        public void CreateCellWithBomb()
+        {
+            var cell = new Cell(true);
+            Assert.IsTrue(cell.HasBomb && cell.Status == StatusEnum.Untouched);
+        }
+
+        [TestMethod]
+        public void CreateCellWithoutBomb()
+        {
+            var cell = new Cell(false);
+            Assert.IsTrue(!cell.HasBomb && cell.Status == StatusEnum.Untouched);
+        }
+
+        [TestMethod]
+        public void FlagCell()
+        {
+            var cell = new Cell(false);
+            cell.Flagged();
+            Assert.AreEqual(cell.Status, StatusEnum.Flagged);
+        }
+
+        [TestMethod]
+        public void CheckCellWithoutBomb()
+        {
+            var cell = new Cell(false);
+            cell.Check();
+            Assert.AreEqual(cell.Status, StatusEnum.Revealed);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void CheckCellWithBomb()
+        {
+            var cell = new Cell(true);
+            cell.Check();
+        }
+    }
+}
