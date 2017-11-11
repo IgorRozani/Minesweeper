8e7af632f614f9c43e60bbcdfdb50764cd6ac88c
Igor Rozani <igorrozani@gmail.com>
2017-11-11 02:03:20 -0200

    Levels test

---
+++ Minesweeper/Minesweeper.Test/Domain/Level/HardLevel/Unit/HardLevelTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Minesweeper.Test.Domain.Level.HardLevel.Unit
+{
+    [TestFixture]
+    public class HardLevelTest
+    {
+        private Minesweeper.Domain.Level.HardLevel _hardLevel;
+
+        [SetUp]
+        public void InitializeTests()
+        {
+            _hardLevel = new Minesweeper.Domain.Level.HardLevel();
+        }
+
+        [Test]
+        public void QuantityRows_Return30()
+        {
+            var quantityRows = _hardLevel.QuantityRows();
+            quantityRows.Should().Be(30);
+        }
+
+        [Test]
+        public void QuantityCollumns_Return30()
+        {
+            var quantityCollumns = _hardLevel.QuantityCollumns();
+            quantityCollumns.Should().Be(30);
+        }
+    }
+}
